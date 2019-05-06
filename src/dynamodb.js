e5303a25d8e020765e73ef6fcbde9a4deb30c9a8
mmniazi <muhammad.mohsin1994@gmail.com>
2019-05-06 23:06:55 +0100

    setup local lambda and dynamodb

--- src/dynamodb.js
+++ src/dynamodb.js
@@ -1,10 +1,36 @@
+const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies, global-require
+
 const MAX_STREAMS = 3;
 const MIN_STREAMS = 0;
 const DYNAMO_ERROR = 'Failed to fetch active streams count';
 const CONDITION_EXCEPTION = 'ConditionalCheckFailedException';
 
 function initDynamo() {
-  const AWS = require('aws-sdk'); // eslint-disable-line import/no-extraneous-dependencies, global-require
+  return new AWS.DynamoDB.DocumentClient();
+}
+
+function initDynamoLocal() {
+  AWS.config.update({
+    endpoint: 'http://localhost:8000',
+    region: 'eu-west-2',
+  });
+
+  const dynamodb = new AWS.DynamoDB();
+
+  const params = {
+    TableName: 'streaming-api-dev',
+    KeySchema: [{ AttributeName: 'id', KeyType: 'HASH' }],
+    AttributeDefinitions: [{ AttributeName: 'id', AttributeType: 'S' }],
+    ProvisionedThroughput: { ReadCapacityUnits: 1, WriteCapacityUnits: 1 },
+  };
+
+  dynamodb.createTable(params, (err, data) => {
+    if (err && err.code !== 'ResourceInUseException') {
+      console.error('Unable to create table. Error JSON:', JSON.stringify(err, null, 2));
+    } else if (!err) {
+      console.log('Created table. Table description JSON:', JSON.stringify(data, null, 2));
+    }
+  });
 
   return new AWS.DynamoDB.DocumentClient();
 }
@@ -35,6 +61,7 @@ function decrCounterParams(userId) {
 
 module.exports = {
   initDynamo,
+  initDynamoLocal,
   incrCounterParams,
   decrCounterParams,
   DYNAMO_ERROR,
--- src/stream.js
+++ src/stream.js
@@ -1,8 +1,8 @@
-const { initDynamo } = require('./dynamodb');
+const { initDynamo, initDynamoLocal } = require('./dynamodb');
 const { start } = require('./routes/start');
 const { end } = require('./routes/end');
 
-const dynamoDb = initDynamo();
+const dynamoDb = process.env.NODE_ENV === 'development' ? initDynamoLocal() : initDynamo();
 
 module.exports.start = start(dynamoDb);
 module.exports.end = end(dynamoDb);
